8139298a6f4550d28e5adf0aa7323a1fe3ca9f13
clara <me@me.com>
2019-02-14 17:23:05 -0600

    social media

---
+++ src/main/java/numbers/CountPixels.java
@@ -0,0 +1,28 @@
+package numbers;
+
+public class CountPixels {
+
+
+    public static void main(String[] args) {
+
+        int imageHeight = 300;
+        int imageWidth = 400;
+
+        int pixels = totalPixels(imageHeight, imageWidth);
+
+        System.out.println("your image has " + pixels + " pixels");
+    }
+
+    private static int totalPixels(int height, int width) {
+
+        // TODO calculate the total pixels in the image, by multiplying the height and width
+
+        return 0;
+
+    }
+
+
+}
+
+
+
