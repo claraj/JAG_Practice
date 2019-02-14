a0826f7e612de91b9da7f51cc8499b4661278d48
clara <me@me.com>
2019-02-14 17:35:20 -0600

    hello loops

---
+++ src/main/java/loops/HelloLoops_For.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class HelloLoops_For {
+
+    public static void main(String[] args) {
+
+        printOneToTen();
+
+    }
+
+    private static void printOneToTen() {
+
+        // TODO finish this method to print the numbers 1 to 10.
+        // Use a for loop.
+
+    }
+
+}
---
+++ src/main/java/loops/HelloLoops_While.java
@@ -0,0 +1,21 @@
+package loops;
+
+public class HelloLoops_While {
+
+    public static void main(String[] args) {
+
+        printOneToTen();
+
+    }
+
+    private static void printOneToTen() {
+
+        // TODO finish this method to print the numbers 1 to 10.
+        //
+        // Use a * WHILE * loop.
+        //
+        // You'll need to create a variable to be the loop counter, and think about what the condition should be.
+
+    }
+
+}
